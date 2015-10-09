32760faab9520079475fdb3952c64d4e6c16d16d
neoedmund <neoedmund@gmail.com>
2015-10-09 17:12:45 +0800

    add java interface for open with a string by 3rd java application

--- src/neoe/ne/Main.java
+++ src/neoe/ne/Main.java
@@ -1,15 +1,12 @@
 package neoe.ne;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-                Plugin.load();
-		U.Config.setDefaultLookAndFeel();
-		U.Config.setDefaultBKColor();
-		U.Config.initKeys();
-                Ime.loadImes();
+		doinit();
 		EditPanel editor = new EditPanel();
 		if (args.length > 0) {
 			File f = new File(args[0]);
@@ -26,8 +23,25 @@ public class Main {
 		SwingJniJvmPatch();
 	}
 
+	public static EditPanel open(String title, String text) throws Exception {
+		doinit();
+		EditPanel editor = new EditPanel();
+		PlainPage pp = new PlainPage(editor, PageData.newEmpty(title, ""));
+		pp.ptEdit.append(text);
+		editor.openWindow();		
+		return editor;
+	}
+
+	private static void doinit() throws Exception {
+		Plugin.load();
+		U.Config.setDefaultLookAndFeel();
+		U.Config.setDefaultBKColor();
+		U.Config.initKeys();
+		Ime.loadImes();
+	}
+
 	/**
-	 * something like said in https://forums.oracle.com/thread/1542114 
+	 * something like said in https://forums.oracle.com/thread/1542114
 	 */
 	private static void SwingJniJvmPatch() {
 		while (true) {
--- src/neoe/ne/Version.java
+++ src/neoe/ne/Version.java
@@ -2,6 +2,6 @@ package neoe.ne;
 
 public class Version {
 	// use git instead of svn , so change rxxx to vxxx
-	static final String REV = "v244";
+	static final String REV = "v245";
 	public static final String CONFIG_FN = "/data.py.ver7";
 }
