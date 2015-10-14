3fcec240eeea89c2fad6f4b6dae927ce8e54433c
neoedmund <neoedmund@gmail.com>
2015-10-14 13:45:12 +0800

    move left/right word, now goto prev/next line instead of stay there.

--- src/neoe/ne/Version.java
+++ src/neoe/ne/Version.java
@@ -2,6 +2,6 @@ package neoe.ne;
 
 public class Version {
 	// use git instead of svn , so change rxxx to vxxx
-	static final String REV = "v245";
+	static final String REV = "v246";
 	public static final String CONFIG_FN = "/data.py.ver7";
 }
