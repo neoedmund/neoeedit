cd2180c151b668a1faa0cb2c1e928ad07c21d522
neoedmund <neoedmund@gmail.com>
2015-12-04 14:02:48 +0800

    regist?

---
+++ src/neoe/ne/Regist.java
@@ -0,0 +1,14 @@
+package neoe.ne;
+
+public class Regist {
+
+	public static void main(String[] args) throws Exception {
+		regist(args[0], args[1]);
+	}
+
+	private static void regist(String cls, String staticField) throws Exception {
+		ClassLoader cl = Regist.class.getClassLoader();
+		cl.loadClass(cls).getField(staticField).set(null, Main.class);
+	}
+
+}
