87eaf7ee3537c9af5a0850935ba3d4b67c5a92f8
neoedmund <neoedmund@gmail.com>
2015-12-31 10:57:33 +0800

    add a script: delete comments in config files

---
+++ scripts/neoeedit/DelCommetSemi1.java
@@ -0,0 +1,27 @@
+package neoeedit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import neoe.ne.Script;
+
+public class DelCommetSemi1 implements Script {
+
+	@Override
+	public List<CharSequence> run(List<CharSequence> lines) {
+		final char COMMENT_CHAR = ';'; //if not #
+		List<CharSequence> ret = new ArrayList<CharSequence>();
+		int emptyLine= 0;
+		for (CharSequence line : lines) {
+			if (line.length()>0 && line.charAt(0) == COMMENT_CHAR) continue;
+			if (line.length()==0){
+				emptyLine++;
+			}else{
+				emptyLine=0;
+			}
+			if (emptyLine<=3)
+				ret.add(line);
+		}
+		return ret;
+	}
+}
