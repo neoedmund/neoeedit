35039bf6b3dbb48e1c487434bc6f2e35caec807a
neoe <neoedmund@gmail.com>
2018-03-23 15:50:19 +0800

    add keywords for lua

--- src/neoe/ne/Version.java
+++ src/neoe/ne/Version.java
@@ -2,7 +2,7 @@ package neoe.ne;
 
 public class Version {
  
-	public static final String REV = "v305".toString(); // β
+	public static final String REV = "v306".toString(); // β
  
 	public static final String CONFIG_FN = "/data.py.ver17".toString();
 }
