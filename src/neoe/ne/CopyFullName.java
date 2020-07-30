b1f32f2904a212ea9a36a2a09578ecc6ff3455fa
neoe <neoedmund@gmail.com>
2020-07-30 09:27:19 +0800

    fix for Clipboard data is lost on exit

--- src/neoe/ne/CopyFullName.java
+++ src/neoe/ne/CopyFullName.java
@@ -5,8 +5,9 @@ import java.awt.datatransfer.StringSelection;
 
 public class CopyFullName {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(args[0]), null);
+		Thread.sleep(10000); //10 sec window for user to paste, for https://wiki.ubuntu.com/ClipboardPersistence 
 	}
 
 }
