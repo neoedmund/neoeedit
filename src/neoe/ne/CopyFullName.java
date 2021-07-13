cd830e611711696f151aa44140bd7e6204b573a5
neoe <hi2@n101n.xyz>
2021-07-13 11:34:18 +0900

    tweak filename copied

--- src/neoe/ne/CopyFullName.java
+++ src/neoe/ne/CopyFullName.java
@@ -6,7 +6,8 @@ import java.awt.datatransfer.StringSelection;
 public class CopyFullName {
 
 	public static void main(String[] args) throws Exception {
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(args[0]), null);
+		String fn = args[0].replace('\\', '/');
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(fn), null);
 		Thread.sleep(10000); // 10 sec window for user to paste, for
 								// https://wiki.ubuntu.com/ClipboardPersistence
 	}
