fa5b1f7b7d6ec905f5c4f2c6a240ca2b2c6c681d
neoe <hi2@n101n.xyz>
2021-07-25 13:32:52 +0900

    PicView bugfix and add mouse position indicator

--- src/neoe/ne/PicView.java
+++ src/neoe/ne/PicView.java
@@ -115,18 +115,19 @@ public class PicView {
 		private int pw;
 		double rate = 1.0;
 		private boolean small = true;
-		private int vx;
+		private double vx, vx1, vy, vy1;
 
-		private int vx1;
-
-		private int vy;
-		private int vy1;
 		Rectangle maxWindow;
 
 		boolean superMode = false;
 		private Iterator<File> sfi;
 		List<File> superModeHistory = new LinkedList<>();
 		int superModeHistoryPointer = 0;
+
+		boolean needRepaint;
+
+		int rx, ry;
+		boolean drawMousePos = true;
 
 		public PicViewPanel(JFrame f, File fn) throws IOException {
 			this.frame = f;
@@ -145,6 +146,22 @@ public class PicView {
 			addMouseWheelListener(this);
 			addKeyListener(this);
 			setFocusable(true);
+			Thread repaintThread = new Thread(() -> {
+				while (true) {
+					if (needRepaint) {
+						needRepaint = false;
+						repaint();
+						needRepaint = false;
+					}
+					try {
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			repaintThread.setDaemon(true);
+			repaintThread.start();
 		}
 
 		@Override
@@ -152,18 +169,13 @@ public class PicView {
 			int kc = e.getKeyCode();
 			try {
 				if (e.isControlDown()) {
-					// if (kc == KeyEvent.VK_H) {
-					// U.openFileHistory();
-					// } else if (kc == KeyEvent.VK_O) {
-					// openFile();
-					// } else
 					if (kc == KeyEvent.VK_W) {
 						frame.dispose();
 					}
 				} else {
 					if (kc == KeyEvent.VK_F1 || kc == KeyEvent.VK_TAB) {
 						small = !small;
-						repaint();
+						repaint1();
 					} else if (kc == KeyEvent.VK_LEFT || kc == KeyEvent.VK_BACK_SPACE) {
 						viewFile(-1);
 					} else if (kc == KeyEvent.VK_RIGHT || kc == KeyEvent.VK_SPACE) {
@@ -184,7 +196,7 @@ public class PicView {
 						rate = 1;
 						vx = 0;
 						vy = 0;
-						repaint();
+						repaint1();
 					}
 				}
 			} catch (Exception e1) {
@@ -201,15 +213,6 @@ public class PicView {
 		@Override
 		public void keyTyped(KeyEvent e) {
 
-		}
-
-		public void toggleSuperMode() {
-			superMode = !superMode;
-			if (superMode && sfi == null) {
-				File dir = f.getParentFile();
-				if (dir != null)
-					sfi = new FileIterator(dir.getAbsolutePath()).iterator();
-			}
 		}
 
 		private List<File> listImgs() {
@@ -236,92 +239,113 @@ public class PicView {
 		public void mouseClicked(MouseEvent e) {
 			int x = e.getX(), y = e.getY();
 			if (e.getClickCount() == 2) {
-				vx = (int) ((vx + x) * rate - x);
-				vy = (int) ((vy + y) * rate - y);
-				rate = 1;
-				repaint();
+				setRate(x, y, 1);
+				repaint1();
 			} else {
 
 			}
+		}
+
+		private void setRate(int x, int y, double r2) {
+			vx = r2 / rate * (vx - x) + x;
+			vy = r2 / rate * (vy - y) + y;
+			rate = r2;
 		}
 
 		@Override
 		public void mouseDragged(MouseEvent e) {
 			int x = e.getX(), y = e.getY();
+			if (inSmall(x, y)) {
+				setPosSmall(x, y);
+			} else {
+				int dx = e.getX() - mx;
+				int dy = e.getY() - my;
+				vx = (int) (vx1 + dx);
+				vy = (int) (vy1 + dy);
+				repaint1();
+			}
+		}
+
+		private boolean inSmall(int x, int y) {
 			int w = getWidth();
 			int h = getHeight();
 			int sw = w / 4;
 			int sh = sw * ph / pw;
-			if (x > w - sw && y > h - sh && small) {
-				vx = (int) ((x - w + sw) * pw / rate / sw - w / 2);
-				vy = (int) ((y - h + sh) * ph / rate / sh - h / 2);
-				repaint();
-			} else {
-				int dx = e.getX() - mx;
-				int dy = e.getY() - my;
-				vx = vx1 - dx;
-				vy = vy1 - dy;
-				repaint();
-			}
+			return x > w - sw && y > h - sh && small;
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
-
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
-
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent e) {
-
+			rx = e.getX();
+			ry = e.getY();
+			repaint1();
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
 			int x = e.getX(), y = e.getY();
+			mx = e.getX();
+			my = e.getY();
+			if (inSmall(x, y)) {
+				setPosSmall(x, y);
+			} else {
+				vx1 = vx;
+				vy1 = vy;
+			}
+		}
+
+		private void setPosSmall(int x, int y) {
 			int w = getWidth();
 			int h = getHeight();
 			int sw = w / 4;
 			int sh = sw * ph / pw;
-			{
-				mx = e.getX();
-				my = e.getY();
-				vx1 = vx;
-				vy1 = vy;
-			}
-			if (x > w - sw && y > h - sh && small) {
-				vx = (int) ((x - w + sw) * pw / rate / sw - w / 2);
-				vy = (int) ((y - h + sh) * ph / rate / sh - h / 2);
-				repaint();
-			} else {
 
-			}
+			// x = (w - sw - vx / rate / w * sw)
+			double x1 = x - sw / rate / 2;
+			double y1 = y - sh / rate / 2;
+			vx = (w - sw - x1) * rate * w / sw;
+			vy = (h - sh - y1) * rate * h / sh;
+			repaint1();
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
-
 		}
 
 		@Override
 		public void mouseWheelMoved(MouseWheelEvent e) {
 			int amount = e.getWheelRotation() * e.getScrollAmount();
 			int x = e.getX(), y = e.getY();
-			if (amount < 0) {
-				rate = rate / 1.1;
-				vx = (int) ((vx + x) * 1.1 - x);
-				vy = (int) ((vy + y) * 1.1 - y);
+			if (amount > 0) {
+				setRate(x, y, rate / 1.1);
 			} else {
-				rate = rate * 1.1;
-				vx = (int) ((vx + x) / 1.1 - x);
-				vy = (int) ((vy + y) / 1.1 - y);
+				setRate(x, y, rate * 1.1);
 			}
-			// rate=rate1;
-			repaint();
+			repaint1();
+		}
+
+		private File nextSpFile() {
+			while (sfi.hasNext()) {
+				File f = sfi.next();
+				if (U.isImageFile(f)) {
+					superModeHistory.add(f);
+					superModeHistoryPointer = superModeHistory.size();
+					if (superModeHistoryPointer > 500) {
+						superModeHistory.remove(0);
+						superModeHistoryPointer--;
+					}
+					return f;
+				}
+			}
+			return null;
 		}
 
 		@Override
@@ -334,28 +358,59 @@ public class PicView {
 				g.setColor(Color.WHITE);
 				g.fillRect(0, 0, w, h);
 			}
-			// System.out.println(w+"x"+h);
 			int sw = w / 4;
 			int sh = sw * ph / pw;
 
-			g.drawImage(img, 0, 0, w, h, (int) (vx * rate), (int) (vy * rate), (int) ((w + vx) * rate),
-					(int) ((h + vy) * rate), null);
+			g.drawImage(img, (int) vx, (int) vy, (int) (pw * rate), (int) (ph * rate), null);
+
 			if (small) {
 				g.drawImage(img, w - sw, h - sh, w, h, 0, 0, pw, ph, null);
 				g.setColor(Color.WHITE);
 				g.drawRect(w - sw, h - sh, sw, sh);
 				g.setColor(Color.RED);
-				g.drawRect((int) (w - sw + vx * rate * sw / pw), //
-						(int) (h - sh + vy * rate * sh / ph), //
-						(int) (sw * w * rate / pw), //
-						(int) (sh * h * rate / ph));
+				g.drawRect((int) (w - sw - vx / rate / w * sw), //
+						(int) (h - sh - vy / rate / w * sw), //
+						(int) (sw / rate), //
+						(int) (sh / rate));
 			}
+			if (drawMousePos) {
+				double x, y;
+				if (inSmall(rx, ry)) {
+					x = (rx - (w - sw)) * pw / sw;
+					y = (ry - (h - sh)) * ph / sh;
+				} else {
+					x = (int) ((-vx + rx) / rate);
+					y = (int) ((-vy + ry) / rate);
+				}
+				String spos = String.format("%d,%d", (int) x, (int) y);
+				int textW = g.getFontMetrics().stringWidth(spos);
+				int textH = g.getFont().getSize();
+				int x2 = rx;
+				int y2 = ry;
+				if (x2 + textW > w) {
+					x2 = w - textW;
+				}
+				if (y2 < textH) {
+					y2 = textH;
+				}
+				g.setColor(Color.BLACK);
+				g.drawString(spos, x2 + 1, y2 + 1);
+				g.setColor(Color.WHITE);
+				g.drawString(spos, x2, y2);
+			}
+			g.dispose();
+
+		}
+
+		public void repaint1() {
+			needRepaint = true;
 		}
 
 		public void rotate(int direction) {
 			int angle = direction * 90;
 			int w = img.getWidth();
 			int h = img.getHeight();
+//			System.out.printf("wh=[%dx%d]\n", w, h);
 			int neww = h, newh = w;
 			BufferedImage dest = new BufferedImage(neww, newh, img.getType());
 			Graphics2D g = dest.createGraphics();
@@ -365,15 +420,64 @@ public class PicView {
 			g.dispose();
 			img = dest;
 			setSize(img);
-			repaint();
+//			vx = 0;vy = 0;
+			repaint1();
 		}
 
 		private void setSize(BufferedImage img) {
-			Dimension dim = new Dimension(pw = (int) (img.getWidth() / rate + 20),
-					ph = (int) (img.getHeight() / rate + 50));
+			Dimension dim = new Dimension(pw = (int) (img.getWidth() + 20), ph = (int) (img.getHeight() + 20));
 			dim.width = Math.min(maxWindow.width, Math.max(200, dim.width));
 			dim.height = Math.min(maxWindow.height, Math.max(200, dim.height));
 			frame.setSize(dim);
+		}
+
+		private void setTitleWithSize(File f, int index, int total) {
+			String ss1 = "";
+			if (ss != null) {
+				ss1 = ss.delay > 0 ? " slide:" + ss.delay + " sec" : "";
+			}
+			frame.setTitle(String.format("PicView %s [%dx%d] %s %,d BS%s - neoeedit %s", f.getName(), img.getWidth(),
+					img.getHeight(), superMode ? "SP" : String.format("(%d/%d)", index + 1, total), f.length(), ss1,
+					Version.REV));
+			setSize(img);
+		}
+
+		/** i only supported to be 1 or -1 */
+		private void superModeViewFile(int i) {
+			if (i > 0) {
+				if (superModeHistoryPointer < superModeHistory.size()) {
+					viewFile(superModeHistory.get(superModeHistoryPointer++));
+				} else {
+					File f = nextSpFile();
+					if (f != null) {
+						viewFile(f);
+					} // else skip
+				}
+			} else if (i < 0) {
+				if (superModeHistoryPointer > 0 && superModeHistoryPointer <= superModeHistory.size()) {
+					viewFile(superModeHistory.get(--superModeHistoryPointer));
+				} // else skip
+			}
+
+		}
+
+		public void toggleSuperMode() {
+			superMode = !superMode;
+			if (superMode && sfi == null) {
+				File dir = f.getParentFile();
+				if (dir != null)
+					sfi = new FileIterator(dir.getAbsolutePath()).iterator();
+			}
+		}
+
+		private void viewFile(File f) {
+			try {
+				img = ImageIO.read(f);
+				setTitleWithSize(f, fi, files.size());
+				repaint1();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 
 		public void viewFile(int i) {
@@ -398,89 +502,6 @@ public class PicView {
 
 		}
 
-		private void viewFile(File f) {
-			try {
-				img = ImageIO.read(f);
-				setTitleWithSize(f, fi, files.size());
-				repaint();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		/** i only supported to be 1 or -1 */
-		private void superModeViewFile(int i) {
-			if (i > 0) {
-				if (superModeHistoryPointer < superModeHistory.size()) {
-					viewFile(superModeHistory.get(superModeHistoryPointer++));
-				} else {
-					File f = nextSpFile();
-					if (f != null) {
-						viewFile(f);
-					} // else skip
-				}
-			} else if (i < 0) {
-				if (superModeHistoryPointer > 0 && superModeHistoryPointer <= superModeHistory.size()) {
-					viewFile(superModeHistory.get(--superModeHistoryPointer));
-				} // else skip
-			}
-
-		}
-
-		private File nextSpFile() {
-			while (sfi.hasNext()) {
-				File f = sfi.next();
-				if (U.isImageFile(f)) {
-					superModeHistory.add(f);
-					superModeHistoryPointer = superModeHistory.size();
-					if (superModeHistoryPointer > 500) {
-						superModeHistory.remove(0);
-						superModeHistoryPointer--;
-					}
-					return f;
-				}
-			}
-			return null;
-		}
-
-		private void setTitleWithSize(File f, int index, int total) {
-			String ss1 = "";
-			if (ss != null) {
-				ss1 = ss.delay > 0 ? " slide:" + ss.delay + " sec" : "";
-			}
-			frame.setTitle(String.format("PicView %s [%dx%d] %s %,d BS%s - neoeedit %s", f.getName(), img.getWidth(),
-					img.getHeight(), superMode ? "SP" : String.format("(%d/%d)", index + 1, total), f.length(), ss1,
-					Version.REV));
-			setSize(img);
-		}
-
-	}
-
-	public static void main(String[] args) throws IOException {
-		new PicView().show(new File(args[0]));
-
-	}
-
-	private EditorPanel ep;
-
-	public PicView() {
-	}
-
-	public PicView(EditorPanel ep) {
-		this.ep = ep;
-	}
-
-	public void show(File fn) throws IOException {
-		JFrame f = new JFrame();
-		f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		f.setIconImage(U.getAppIcon(U.e3_png));
-		PicViewPanel p = new PicViewPanel(f, fn);
-		f.getContentPane().setLayout(new BorderLayout());
-		f.getContentPane().add(p);
-		f.setTransferHandler(new U.TH(ep));
-		f.setVisible(true);
-		U.saveFileHistory(fn.getAbsolutePath(), 0);
-		installSlideshow(f, p);
 	}
 
 	private class Slideshow {
@@ -490,10 +511,6 @@ public class PicView {
 
 		public Slideshow(PicViewPanel p) {
 			this.p = p;
-		}
-
-		public void stop() {
-			delay = 0;
 		}
 
 		public void decDelay() {
@@ -525,9 +542,28 @@ public class PicView {
 			}
 
 		}
+
+		public void stop() {
+			delay = 0;
+		}
+
 	}
 
+	public static void main(String[] args) throws IOException {
+		new PicView().show(new File(args[0]));
+
+	}
+
+	private EditorPanel ep;
+
 	Slideshow ss;
+
+	public PicView() {
+	}
+
+	public PicView(EditorPanel ep) {
+		this.ep = ep;
+	}
 
 	private void installSlideshow(JFrame frame, PicViewPanel p) {
 		ss = new Slideshow(p);
@@ -539,13 +575,13 @@ public class PicView {
 			}
 
 			@Override
-			public void windowIconified(WindowEvent e) {
-				ss.iconed = true;
+			public void windowDeiconified(WindowEvent e) {
+				ss.iconed = false;
 			}
 
 			@Override
-			public void windowDeiconified(WindowEvent e) {
-				ss.iconed = false;
+			public void windowIconified(WindowEvent e) {
+				ss.iconed = true;
 			}
 		});
 		new Thread(() -> {
@@ -556,5 +592,18 @@ public class PicView {
 			}
 		}).start();
 
+	}
+
+	public void show(File fn) throws IOException {
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		f.setIconImage(U.getAppIcon(U.e3_png));
+		PicViewPanel p = new PicViewPanel(f, fn);
+		f.getContentPane().setLayout(new BorderLayout());
+		f.getContentPane().add(p);
+		f.setTransferHandler(new U.TH(ep));
+		f.setVisible(true);
+		U.saveFileHistory(fn.getAbsolutePath(), 0);
+		installSlideshow(f, p);
 	}
 }
--- src/neoe/ne/Version.java
+++ src/neoe/ne/Version.java
@@ -2,7 +2,7 @@ package neoe.ne;
 
 public class Version {
 
-	public static final String REV = "v347".toString(); // β
+	public static final String REV = "v348".toString(); // β
 
 	public static final String CONFIG_FN = "/data.py.ver28".toString();
 }
