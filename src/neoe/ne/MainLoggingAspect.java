6ba7b5e75073c0f36c38272927ad95783d6de6cd
Saxton Dees <saxton.dees@gmail.com>
2025-04-09 19:33:33 -0600

    added to gitignore and some aspectj

---
+++ src/neoe/ne/MainLoggingAspect.java
@@ -0,0 +1,23 @@
+package neoe . ne;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class MainLoggingAspect {
+    @Before("execution(* neoe.ne.Main.doinit(..))")
+    public void beforeDoinit(JoinPoint joinPoint) {
+        System.out.println("Initializing application: " + joinPoint.getSignature());
+    }
+
+    @Before("execution(* neoe.ne.Main.main(..))")
+    public void beforeMain(JoinPoint joinPoint) {
+        System.out.println("Starting application: " + joinPoint.getSignature());
+    }
+
+    @Before("execution(* neoe.ne.Main.openDoc(..))")
+    public void beforeOpenDoc(JoinPoint joinPoint) {
+        System.out.println("Opening document: " + joinPoint.getSignature());
+    }
+}
